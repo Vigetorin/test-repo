57a576b95aeca88586715324209a65dc0d4291ce
Vigetorin <vector.kukushkin@ya.ru>
2024-12-12 18:07:45 +0300

    Тесты для "body_contains"

--- main.cpp
+++ main.cpp
@@ -7,9 +7,77 @@
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
+// тесты для body_contains
+
+// проверка обычной свечи (с тенями и непустым телом)
+bool bodyContainsDefaultTest()
+{
+    Candle candleGreen(0.0, 5.0, -1.0, 3.0);
+    Candle candleRed(3.0, 5.0, -1.0, 0.0);
+    for (const Candle& candle : { candleGreen, candleRed })
+    {
+        // значение внутри тела
+        if (!candle.body_contains(1.2))
+            return false;
+        // значения по краям тела
+        if (!candle.body_contains(0.0) || !candle.body_contains(3.0))
+            return false;
+        // значения внутри теней
+        if (candle.body_contains(-0.5) || candle.body_contains(5.0))
+            return false;
+        // значения вне свечи
+        if (candle.body_contains(-1.7) || candle.body_contains(7.6))
+            return false;
+    }
+    return true;
+}
+
+// проверка свечи с пустым телом
+bool bodyContainsZeroBodyTest()
+{
+    Candle candle(1.0, 5.0, -1.0, 1.0);
+    // проверка единственного значения внутри тела
+    if (!candle.body_contains(1.0))
+        return false;
+    // проверка значений вне тела
+    if (candle.body_contains(0.2) || candle.body_contains(7.3))
+        return false;
+    // пустая свеча без теней
+    Candle candleEmpty(1.0, 1.0, 1.0, 1.0);
+    // значение внутри свечи
+    if (!candleEmpty.body_contains(1.0))
+        return false;
+    // внешние значения
+    if (candleEmpty.body_contains(0.8) || candleEmpty.body_contains(1.7))
+        return false;
+    return true;
+}
+
+// проверка свечи без теней
+bool bodyContainsNoShadowsTest()
+{
+    Candle candleGreen(0.0, 3.0, 0.0, 3.0);
+    Candle candleRed(3.0, 3.0, 0.0, 0.0);
+    for (const Candle& candle : { candleGreen, candleRed })
+    {
+        // значение внутри свечи
+        if (!candle.body_contains(1.2))
+            return false;
+        // значения по краям свечи
+        if (!candle.body_contains(0.0) || !candle.body_contains(3.0))
+            return false;
+        // значения вне свечи
+        if (candle.body_contains(-1.7) || candle.body_contains(7.6))
+            return false;
+    }
+    return true;
+}
+
 void initTests()
 {
-
+    tests.push_back(bodyContainsDefaultTest);
+    tests.push_back(bodyContainsZeroBodyTest);
+    tests.push_back(bodyContainsNoShadowsTest);
 }
 
 int launchTests()
